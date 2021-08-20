42a516aeff52856d737e5bb83019a56548d313ee
Mario Dimitrov <Mario.Dimitrov@indeavr.com>
2021-08-20 16:42:43 +0300

    Add popup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 import './select2.js';
+
+const infoImage = document.getElementById('info-image');
+
+const showPopup = () => {
+    document.querySelector('.popup').classList.toggle('show');
+}
+
+infoImage.addEventListener('click', showPopup);
+
