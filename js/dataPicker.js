3320096ff768f8a3d3c27f4d87ed26b177837a21
Mario Dimitrov <Mario.Dimitrov@indeavr.com>
2021-08-25 14:06:26 +0300

    Add data picker

---
+++ js/dataPicker.js
@@ -0,0 +1,9 @@
+$("#calendar").flatpickr({
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+    "locale": "he",
+    disableMobile: "true",
+    // nextArrow: '>>',
+    position: 'auto right',
+});
